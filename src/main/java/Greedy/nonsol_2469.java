6147d186d8be9cf818b31b14ba89216fd4329596
Ojimin <ogm0205@gmail.com>
2025-05-24 10:44:48 +0900

    temp

--- src/main/java/Greedy/nonsol_2469.java
+++ src/main/java/Greedy/nonsol_2469.java
@@ -1,9 +1,16 @@
 package Greedy;
 
+import java.io.IOException;
+
 // 2469 - 사다리 타기
 // k명 참가자들이 사다리 타기를 통해 순서 결정
 // 출력 : 가로 막대를 적절히 넣어서 참가자들의 최종 순서가 원하는 순서대로 나오도록
 // 가로 막대가 없는 경우 *, 있을경우 -, 감추어진 특정 가로 줄은 길이 k-1인 ? 문자열로 표시
 public class nonsol_2469 {
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
 
 }
