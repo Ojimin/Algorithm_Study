d859ba6c37f5ee5993eea298708acca143ad93cc
Ojimin <ogm0205@gmail.com>
2025-05-26 14:48:53 +0900

    5/24

---
+++ src/main/java/문자열/sol_9536.java
@@ -0,0 +1,49 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// 9536 - 여우는 어떻게 울지?
+// 태스트케이스 개수 입력 -> 여우 제외 동물 goes 소리  * 울음소리는 한 단어
+public class sol_9536 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        String sentence, str;
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<T; i++) {
+            sentence = br.readLine();
+            st = new StringTokenizer(sentence);
+            ArrayList<String> soundList = new ArrayList<>();
+            Map<String, String> animalSound = new HashMap<>();
+            while (st.hasMoreTokens()) {
+                str = st.nextToken();
+                soundList.add(str);
+            }
+
+            while ((str = br.readLine()) != null && !str.isEmpty() ) {
+                if (str.equals("what does the fox say?")) break;
+                st = new StringTokenizer(str);
+                String animal = st.nextToken();
+                st.nextToken();
+                String sound = st.nextToken();
+                animalSound.put(sound, animal);
+            }
+
+            for (String s : soundList) {
+                if (animalSound.containsKey(s)) continue;
+                else sb.append(s).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
+
