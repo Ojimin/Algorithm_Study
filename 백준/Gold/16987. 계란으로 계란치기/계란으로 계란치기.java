6c39d2bc682ae74d16feef8b9814a144dc6ef927
Ojimin <ogm0205@gmail.com>
2025-06-01 23:20:04 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 196 ms, Memory: 16652 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -10,21 +10,21 @@ import java.util.StringTokenizer;
 // 종료: 가장 최근에 든 계란이 가장 오른쪽에 위치할 경우 종료
 // 내구도가 0이하 되는 순간 꺠짐, 내구도는 상대 깨는(지는) 계란의 무게만큼 감소함
 // 백트래킹
+// 푸는데 오래걸림. 다시 풀어봄직함
 public class Main {
     static int N;
     static int[][] arr;
-    static int[] tmp; // 깎인 내구도 담아놓는 배열
     static int max = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N][2];
-        tmp = new int[N];
+//        tmp = new int[N];
         for (int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken()); // 내구도
             arr[i][1] = Integer.parseInt(st.nextToken()); // 무게
-            tmp[i] = arr[i][0];
+//            tmp[i] = arr[i][0];
         }
 
         if (N == 1) {
@@ -32,46 +32,47 @@ public class Main {
             System.out.println(max);
             return;
         }
-        dfs(0);
+        dfs(0, 0);
         System.out.println(max);
     }
 
-    public static void dfs(int depth) {
+    public static void dfs(int depth, int cnt) {
         if (depth == N) {
-            int cnt = 0;
-            for (int i=0; i<N; i++) {
-                if (tmp[i] <= 0) cnt++;
-            }
-            if (cnt > max) max = cnt;
+            max = Math.max(max, cnt);
             return;
         }
-        if (tmp[depth] <=0) {
-            dfs(depth+1);
+        // 현재 깨뜨릴려고 하는 egg가 깨져있을 경우 & cnt가 N-1개일 때도 넘김(2개 이상 남아있어야 다른 걸 꺨 수 있기 때문)
+        if (arr[depth][0] <=0 || cnt == N-1) {
+            dfs(depth+1, cnt);
             return;
         }
 
+        int tmp=cnt;
         for (int i=0; i<N; i++) {
-            // tmp[i] 값이 0보다 작으면 넘기기(깨뜨리지 말기)
-            if (depth == i || tmp[i]<=0) continue;
+            // 깨트릴 것과 깨트림 당할 인덱스가 같거나 arr[i] 값이 0보다 작으면(이미 깨져있으면) 넘기기(깨뜨리지 말기)
+            if (depth == i || arr[i][0]<=0) continue;
             countEgg(depth, i);
-            dfs(depth+1);
+            if (arr[depth][0] <= 0) tmp++;
+            if (arr[i][0] <= 0) tmp++;
+            dfs(depth+1, tmp);
             recoverEgg(depth, i);
+            tmp = cnt;
         }
     }
 
     public static void countEgg(int i, int j) {
-        tmp[i] -= arr[j][1];
-        tmp[j] -= arr[i][1];
-        int cnt = 0;
-        for (int k=0; k<N; k++) {
-            if (tmp[k] <= 0) cnt++;
-        }
-        if (cnt > max) max = cnt;
+        arr[i][0] -= arr[j][1];
+        arr[j][0] -= arr[i][1];
+//        int cnt = 0;
+//        for (int k=0; k<N; k++) {
+//            if (arr[k][0] <= 0) cnt++;
+//        }
+//        if (cnt > max) max = cnt;
     }
 
     public static void recoverEgg(int i, int j) {
-        tmp[i] += arr[j][1];
-        tmp[j] += arr[i][1];
+        arr[i][0] += arr[j][1];
+        arr[j][0] += arr[i][1];
     }
 
 }
