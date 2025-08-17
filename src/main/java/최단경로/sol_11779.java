664998eee23b3df187808607bce50a36eb71d88a
Ojimin <ogm0205@gmail.com>
2025-08-17 17:58:24 +0900

    8/16

---
+++ src/main/java/최단경로/sol_11779.java
@@ -0,0 +1,105 @@
+package 최단경로;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 11779 - 최소비용 구하기2
+// n개의 도시 & m개의 버스, A -> B 도시로 가는데 비용 최소화
+// 출력: 특정 출발점-도착점이 주어질때, 최소 비용 & 최소비용을 갖는 경로에 포함되어있는 도시 개수 & 최소 비용을 갖는 경로를 방문하는 도시 순서대로 출력
+// 다시 - (주의) 동일 경로에 대해 다른 비용 값 들어올 수 있음
+public class sol_11779 {
+    static class Node {
+        int idx;
+        int cost;
+
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+    }
+    static Deque<Integer> deque;
+    static int n, m;
+    static int[][] graph;
+    static int[] dist;
+    static int[] prev;
+    static int start, end;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        graph = new int[n+1][n+1];
+        dist = new int[n+1];
+        prev = new int[n+1];
+        sb = new StringBuilder();
+        deque = new ArrayDeque<>();
+        for (int i=0; i<=n; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+        }
+        // 중복돼서 나오나?
+        for (int i=0; i<m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph[a][b] = Math.min(graph[a][b], cost);
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        dijkstra();
+        traceRoute();
+        sb.append(dist[end]).append("\n");
+        sb.append(deque.size()).append("\n");
+
+        while(!deque.isEmpty()) {
+            sb.append(deque.pollLast()+" ");
+        }
+        System.out.print(sb);
+    }
+
+    public static void dijkstra() {
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.cost - o2.cost;
+            }
+        });
+        boolean[] check = new boolean[n+1];
+        Arrays.fill(dist, 1_000_000_000);
+        dist[start] = 0;
+        pq.offer(new Node(start, 0));
+
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+            int nowIdx = now.idx;
+            if (check[nowIdx]) continue; // 주의
+            check[nowIdx] = true;
+
+            for (int i=1; i<=n; i++) {
+                if (graph[nowIdx][i] == Integer.MAX_VALUE) continue;
+                if (dist[i] > dist[nowIdx] + graph[nowIdx][i]) {
+                    dist[i] = dist[nowIdx] + graph[nowIdx][i];
+                    prev[i] = nowIdx;
+                    pq.offer(new Node(i, dist[i])); // 주의
+                }
+            }
+        }
+    }
+
+    public static void traceRoute() {
+        deque.offer(end);
+        int prevIdx = prev[end];
+        while(true) {
+            deque.offer(prevIdx);
+            if(prevIdx==start) {
+                break;
+            }
+            prevIdx = prev[prevIdx];
+        }
+    }
+}
---
+++ src/main/java/최단경로/sol_1238.java
@@ -0,0 +1,86 @@
+package 최단경로;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 1238 - 파티
+// N개의 숫자로 구분된 각 마을에 한명의 학생이 살고있음==N명
+// N명 학생이 X번 마을에 모여서 파티.  M개의 단방향 도로들 i번째 길을 지나는데 Ti의 사간을 소비함
+// 도로들은 모두 단방향, N명의 학생들 중 오고 가는데 가장 많은 시간 소비하는 학생 누구?
+// 출력: N명의 학생중, X번 마을까지 오고가는데(왕복) 가장 오래 걸린 학생의 소요시간
+// A->B로 가는 도로는 최대 1개로 제한
+// 다시 : 역방향 그래프!!!! => 2번만 다익스트라 계산해서 최단거리 계산 가능
+public class sol_1238 {
+    static class Node {
+        int idx;
+        int cost;
+
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+    }
+    static int N, M, X;
+    static int[] distX;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+        ArrayList<Node>[] graph = new ArrayList[N+1];
+        ArrayList<Node>[] reGraph = new ArrayList[N+1];
+        int answer = Integer.MIN_VALUE;
+        for (int i=0; i<=N; i++) {
+            graph[i] = new ArrayList<>();
+            reGraph[i] = new ArrayList<>();
+        }
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph[a].add(new Node(b, cost));
+            reGraph[b].add(new Node(a, cost));
+        }
+
+        distX = new int[N+1];
+        int dist[] = new int[N+1];
+        dijkstra(X, distX, graph);
+        dijkstra(X, dist, reGraph);
+        for (int i=1; i<=N; i++) {
+            int time = dist[i] + distX[i];
+            answer = Math.max(answer, time);
+        }
+        System.out.println(answer);
+    }
+
+    public static void dijkstra(int start, int[] dist, ArrayList<Node>[] graph) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.cost - o2.cost;
+            }
+        });
+        boolean[] check = new boolean[N+1];
+        pq.offer(new Node(start, 0));
+        Arrays.fill(dist, 1_000_000_000);
+        dist[start] = 0;
+
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+            int nowIdx = now.idx;
+            if(check[nowIdx]) continue;
+            check[nowIdx] = true;
+
+            for (Node next : graph[nowIdx]) {
+                if (dist[next.idx] > dist[nowIdx] + next.cost) {
+                    dist[next.idx] = dist[nowIdx] + next.cost;
+                    pq.offer(new Node(next.idx, dist[next.idx]));
+                }
+            }
+        }
+    }
+}
