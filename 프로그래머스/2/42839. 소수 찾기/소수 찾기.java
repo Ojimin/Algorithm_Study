c1b6f9a2ab838432efea2a32bfd15a676295f2ff
Ojimin <ogm0205@gmail.com>
2025-08-27 16:55:43 +0900

    [level 2] Title: 소수 찾기, Time: 99.37 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -7,23 +7,36 @@ import java.util.*;
 class Solution {
     static int depth;
     static HashSet<Integer> set = new HashSet<>();
-    static boolean[] visited;
+    static boolean[] visited, isPrime;
     public int solution(String numbers) {
         int answer = 0;
         depth = numbers.length();
+        isPrime = new boolean[10000000];
+        Arrays.fill(isPrime, true);
+        //에라토스테네스 체 활용
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for (int i = 2; i<Math.sqrt(9999999); i++) {
+            if(isPrime[i]) {
+                for (int j=i*i; j<=9999999; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
         for (int i=1; i<=depth; i++) {
             visited = new boolean[depth];
             dfs(0, 0, "", i, numbers);
         }
-        for (int i: set) {
+        for(int i: set) {
             System.out.println(i);
         }
+        
         answer = set.size();
         return answer;
     }
     public void dfs(int level, int start, String tmp, int max, String numbers) {
         if (level == max) {
-            if (checkPrime(tmp)) set.add(Integer.parseInt(tmp));
+            if (isPrime[Integer.parseInt(tmp)]) set.add(Integer.parseInt(tmp));
             return;
         }
         for (int i=0; i<depth; i++) {
@@ -34,13 +47,13 @@ class Solution {
             }
         }
     }
+    // 그냥 완탐
     public boolean checkPrime(String str) {
         int num = Integer.parseInt(str);
         if (num == 0 || num == 1) return false;
-        for (int i=2; i<num; i++) {
+        for (int i=2; i<num; i++) { // 굳이 num까지 계산 안해도됨. sqrt로 고고
             if(num % i == 0) return false;
         }
         return true;
     }
-    
 }
