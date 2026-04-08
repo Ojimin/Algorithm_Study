91330839d5c39199fdb439848fa013480907951d
Ojimin <ogm0205@gmail.com>
2026-04-09 01:06:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 145740 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,14 +1,17 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 // 11724 - 연결 요소의 개수
 // 출력: 연결 요소의 개수
 // 하나로 연결되어있는지 판단하는 건데 dfs?
+// 해당 문제는 인접리스트 형태가 더 시간복잡도 낮음.
 public class Main {
     static int N, M;
     static int[][] map;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,19 +19,24 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map= new int[N+1][N+1];
+        graph= new ArrayList[N+1];
         visited = new boolean[N+1];
         int cnt = 0;
+        for (int i=1; i<=N; i++) {
+            graph[i] = new ArrayList<>();
+        }
         for (int i=0;i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             map[x][y] = 1;
             map[y][x] = 1; //방향 없는 그래프이므로
+            graph[x].add(y);
+            graph[y].add(x);
         }
 
         for (int i=1;i<=N; i++) {
             if (!visited[i]) {
-                visited[i]=true;
                 dfs(i);
                 cnt++;
             }
@@ -38,10 +46,10 @@ public class Main {
     }
 
     public static void dfs(int x) {
-        for (int i=1; i<=N; i++) {
-            if(map[x][i]==1 && !visited[i]) {
-                visited[i] = true;
-                dfs(i);
+        visited[x]=true;
+        for (int y: graph[x]) {
+            if(!visited[y]) {
+                dfs(y);
             }
         }
 
